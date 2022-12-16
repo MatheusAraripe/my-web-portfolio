6ed914c7bfa3a52a6dd42baee538bc20675f2025
Matheus Araripe <matheus.alc2001@gmail.com>
2022-12-16 08:18:14 -0300

    feat: make the navbar responsive

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "./header.css";
 
 const Header = () => {
+
+  const[Toggle, showMenu] = useState(false);
+
   return(
     <header className="header">
       <nav className="nav container">
         <a href="index.html" className="nav__logo">Matheus Araripe</a>
 
-        <div className="nav__menu">
+        <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
           <ul className="nav__list grid">
 
             <li className="nav__item">
@@ -54,10 +57,10 @@ const Header = () => {
 
           </ul>
 
-          <i className="uil uil-times nav__close"></i>
+          <i className="uil uil-times nav__close" onClick={() => showMenu(!Toggle)}></i>
         </div>
 
-        <div className="nav__toggle">
+        <div className="nav__toggle" onClick={() => showMenu(!Toggle)}>
           <i className="uil uil-apps"></i>
         </div>
       </nav>
