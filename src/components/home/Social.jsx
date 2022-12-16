1457d350f69c12cac2235496e37d3d25252a33c5
Matheus Araripe <matheus.alc2001@gmail.com>
2022-12-16 10:05:45 -0300

    feat: add the home section

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a href="https://github.com/MatheusAraripe" className="home__social-icon" target="_blank">
+        <i class="uil uil-github-alt"></i>
+      </a>
+
+      <a href="https://www.linkedin.com/in/matheus-araripe/" className="home__social-icon" target="_blank">
+      <i class="uil uil-linkedin-alt"></i>
+      </a>
+
+    </div>
+  )
+}
+
+export default Social
