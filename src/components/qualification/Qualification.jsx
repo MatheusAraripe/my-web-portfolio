8cf185b4fe6dd8d9501b53b7770b4cc7dd4c4f26
Matheus Araripe <matheus.alc2001@gmail.com>
2022-12-22 14:01:15 -0300

    feat: create the qualification section

---
+++ src/components/qualification/Qualification.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './qualification.css';
+
+const Qualification = () => {
+  return (
+    <section className="qualification section">
+      <h2 className="section__title">Qualifications</h2>
+      <span className="section__subtitle">My journey</span>
+    </section>
+  )
+}
+
+export default Qualification
