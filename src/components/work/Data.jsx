c823346c3773fbd8e23c4a2385b56d69735a91a7
Matheus Araripe <matheus.alc2001@gmail.com>
2023-01-05 00:00:30 -0300

    feat: add the projects section

---
+++ src/components/work/Data.jsx
@@ -0,0 +1,45 @@
+import Talentum from '../../assets/talentum-demo.png';
+import Api from '../../assets/print_api.png';
+import Cryptopedia from '../../assets/cryptopedia_demo.png';
+import Mouse from '../../assets/mouse_rand_demo.png';
+
+
+
+export const projectsData = [
+  {
+    title: 'My own news API',
+      image: Api,
+      category: "backend",
+    id: 0,
+  },
+  {
+    title: 'Talentum',
+    image: Talentum,
+    category: "fullstack",
+    id: 1,
+  },
+  {
+    title: 'Mouse Randomization app',
+      image: Mouse,
+      category: 'fullstack',
+    id: 2,
+  },
+  {
+    title: 'Cryptopedia',
+    image: Cryptopedia,
+    category: 'frontend',
+    id: 3,
+  },
+];
+
+export const projectsNav = [
+  {
+    name: 'backend',
+  },
+  {
+    name: 'fullstack',
+  },
+  {
+    name: 'frontend',
+  }
+]
